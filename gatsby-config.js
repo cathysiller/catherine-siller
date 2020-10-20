6349ffc9b772433b613bce11fb79d67b129af470
Catherine Siller <csiller@mailgun.com>
2020-10-19 21:21:32 -0500

    lets see what this does

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  plugins: [
-    `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-theme-blog`,
-      options: {},
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/content/posts`,
-      },
-    },
-  ],
-  // Customize your site metadata:
-  siteMetadata: {
-    title: `Catherine Siller Blog`,
-    author: `Catherine Siller`,
-    description: `My site description...`,
-    social: [
+    plugins: [
+      `gatsby-plugin-netlify-cms`,
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        resolve: `gatsby-theme-blog`,
+        options: {},
       },
       {
-        name: `github`,
-        url: `https://github.com/cathysiller`,
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `markdown-pages`,
+          path: `${__dirname}/content/posts/blog`,
+        },
       },
     ],
-  },
-}
+    // Customize your site metadata:
+    siteMetadata: {
+      title: `Catherine Siller Blog`,
+      author: `Catherine Siller`,
+      description: `Catherine Siller's Blog`,
+      social: [
+        {
+          name: `twitter`,
+          url: `https://twitter.com/gatsbyjs`,
+        },
+        {
+          name: `github`,
+          url: `https://github.com/cathysiller`,
+        },
+      ],
+    },
+  }
